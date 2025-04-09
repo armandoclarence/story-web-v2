8a3742f99eb3736e5fa15d40e145ef56d9825c3a
armandoclarence <aclarence200@gmail.com>
2025-04-09 19:20:22 +0700

    fix baseUrl

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
   root: resolve(__dirname, 'src'),
   publicDir: resolve(__dirname, 'src', 'public'),
+  base: './',
   plugins: [
     VitePWA({
       strategies: 'injectManifest',
